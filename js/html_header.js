3a5f387a8da7bae55861e6d7ca06364536bba280
abdullah@B450-AORUS-PRO-WIFI <abdullah.y.alnafisah@gmail.com>
2025-08-15 10:07:09 +0300

    test relative path for bar links edge case

--- js/html_header.js
+++ js/html_header.js
@@ -1,13 +1,12 @@
 let commonHeader = `
-<header class="header" style="padding: 20px; background-color:#f9f9f9; box-shadow: 0 2px 6px rgba(0,0,0,0.1);">
-<nav class="nav" style="text-align: center; margin-top: 20px;">
-    <ul style="list-style: none; padding: 0; margin: 0;">
-        <li style="display: inline; margin: 0 10px;"><a href="index.html">Home</a></li>
-        <li style="display: inline; margin: 0 10px;"><a href="publications.html">Publications</a></li>
-        <li style="display: inline; margin: 0 10px;"><a href="ftn/ftn.html">FTN</a></li>
-        <li style="display: inline; margin: 0 10px;"><a href="archive.html">Archive</a></li>
+<header class="header" style="padding:20px; background-color:#f9f9f9; box-shadow:0 2px 6px rgba(0,0,0,0.1);">
+  <nav class="nav" style="text-align:center; margin-top:20px;">
+    <ul style="list-style:none; padding:0; margin:0;">
+      <li style="display:inline; margin:0 10px;"><a href="/index.html">Home</a></li>
+      <li style="display:inline; margin:0 10px;"><a href="/publications.html">Publications</a></li>
+      <li style="display:inline; margin:0 10px;"><a href="/ftn/ftn.html">FTN</a></li>
+      <li style="display:inline; margin:0 10px;"><a href="/archive.html">Archive</a></li>
     </ul>
-</nav>
+  </nav>
 </header>
 `;
-document.getElementById("common-header").innerHTML = commonHeader;
